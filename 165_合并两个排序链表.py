c909bcfffb96280ba71120501ee180738cee3092
krisxzc <31032675+krisxzc@users.noreply.github.com>
2018-07-16 20:47:55 +0800

    Update 165_合并两个排序链表.py

--- 165_合并两个排序链表.py
+++ 165_合并两个排序链表.py
@@ -1,5 +1,43 @@
 """
 将两个排序链表合并为一个新的排序链表
 
-给出 1->3->8->11->15->null，2->null， 返回 1->2->3->8->11->15->null。
+给出 1->3->8->11->15->null
+      2->null
+返回 1->2->3->8->11->15->null
 """
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param l1: ListNode l1 is the head of the linked list
+    @param l2: ListNode l2 is the head of the linked list
+    @return: ListNode head of linked list
+    """
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(-1)
+        temp = dummy
+        
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        
+        # 考虑尾巴
+        if l1 is None:
+            temp.next = l2
+        else:
+            temp.next = l1
+        
+        return dummy.next
+
