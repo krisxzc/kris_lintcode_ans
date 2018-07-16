3605fa39b197a6f9ca8d4996046cd8b61567f4a2
krisxzc <31032675+krisxzc@users.noreply.github.com>
2018-07-16 11:45:46 +0800

    Create 76_最长上升子序列.py

---
+++ 76_最长上升子序列.py
@@ -0,0 +1,21 @@
+"""
+给定一个整数序列，找到最长上升子序列（LIS），返回LIS的长度。
+
+定义：最长上升子序列问题是在一个无序的给定序列中找到一个尽可能长的由低到高排列的子序列，这种子序列不一定是连续的或者唯一的
+
+给出 [5,4,1,2,3]，LIS 是 [1,2,3]，返回 3
+给出 [4,2,4,5,3,7]，LIS 是 [2,4,5,7]，返回 4
+
+挑战：要求时间复杂度为O(n^2) 或者 O(nlogn)
+"""
+
+##### 动态规划 
+# 腾讯暑期实习一面题，没答上来被刷了
+
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        # write your code here
