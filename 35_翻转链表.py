cde2fc360370a0efda2526d97614cd2774d8b294
krisxzc <31032675+krisxzc@users.noreply.github.com>
2018-07-16 13:50:03 +0800

    Create 35_翻转链表.py

---
+++ 35_翻转链表.py
@@ -0,0 +1,31 @@
+"""
+翻转一个链表:给出一个链表1->2->3->null，这个翻转后的链表为3->2->1->null
+挑战：在原地一次翻转完成
+"""
+
+"""
+Definition of ListNode
+
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+
+# 翻转链表 基础中的基础
+class Solution:
+    """
+    @param head: n
+    @return: The new head of reversed linked list.
+    """
+    def reverse(self, head):
+        # write your code here
+        cur = None
+        while head is not None:
+            temp = head.next
+            head.next = cur
+            cur = head
+            head = temp
+        return cur
