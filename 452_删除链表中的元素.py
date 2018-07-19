dcbdd9ee0b2d52ec2ae194a2a55af3af68e66dc6
krisxzc <31032675+krisxzc@users.noreply.github.com>
2018-07-19 09:53:04 +0800

    Create 452_删除链表中的元素.py

---
+++ 452_删除链表中的元素.py
@@ -0,0 +1,34 @@
+"""
+删除链表中等于给定值val的所有节点。
+
+给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。
+"""
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @param val: An integer
+    @return: a ListNode
+    """
+    def removeElements(self, head, val):
+        # write your code here
+        
+        #if head is None:
+        #    return head
+        
+        dummy = ListNode(-1, next=head)
+        p = dummy
+        while p.next is not None:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return dummy.next    
